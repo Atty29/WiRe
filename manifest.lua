214e7a515119f7adcfd4a47822e528d5fb663208
Atty29 <46417795+Atty29@users.noreply.github.com>
2026-08-08 20:08:44 +0100

    release: align manifest with 3.3.7-dev

--- manifest.lua
+++ manifest.lua
@@ -3,7 +3,7 @@
 
 return {
   name = "WiRe Rewired",
-  version = "3.3.6-dev",
+  version = "3.3.7-dev",
   channel = "development",
   branch = "development",
   baseUrl = "https://raw.githubusercontent.com/Atty29/WiRe/development/",
@@ -27,58 +27,12 @@ return {
     { src = "README.md", dest = "wire/README.md" },
     { src = "docs/TESTING.md", dest = "wire/TESTING.md" },
   },
-
   packages = {
-    server = {
-      label = "WiRe Rewired Server",
-      component = "server",
-      files = {
-        { src = "server/main.lua", dest = "wire/server.lua" },
-      },
-    },
-
-    client = {
-      label = "WiRe Rewired Client",
-      component = "client",
-      files = {
-        { src = "client/main.lua", dest = "wire/client.lua" },
-      },
-    },
-
-    trigger = {
-      label = "WiRe Rewired Trigger",
-      component = "trigger",
-      files = {
-        { src = "trigger/main.lua", dest = "wire/trigger.lua" },
-      },
-    },
-
-    sensor = {
-      label = "WiRe Rewired Sensor",
-      component = "sensor",
-      files = {
-        { src = "sensor/main.lua", dest = "wire/sensor.lua" },
-      },
-    },
-
-    tablet = {
-      label = "WiRe Rewired Tablet",
-      component = "tablet",
-      files = {
-        { src = "tablet/main.lua", dest = "wire/tablet.lua" },
-      },
-    },
-
-    full = {
-      label = "Full WiRe Rewired Development Package",
-      component = nil,
-      files = {
-        { src = "server/main.lua", dest = "wire/server.lua" },
-        { src = "client/main.lua", dest = "wire/client.lua" },
-        { src = "trigger/main.lua", dest = "wire/trigger.lua" },
-        { src = "sensor/main.lua", dest = "wire/sensor.lua" },
-        { src = "tablet/main.lua", dest = "wire/tablet.lua" },
-      },
-    },
+    server = { label="WiRe Rewired Server", component="server", files={{src="server/main.lua",dest="wire/server.lua"}} },
+    client = { label="WiRe Rewired Client", component="client", files={{src="client/main.lua",dest="wire/client.lua"}} },
+    trigger = { label="WiRe Rewired Trigger", component="trigger", files={{src="trigger/main.lua",dest="wire/trigger.lua"}} },
+    sensor = { label="WiRe Rewired Sensor", component="sensor", files={{src="sensor/main.lua",dest="wire/sensor.lua"}} },
+    tablet = { label="WiRe Rewired Tablet", component="tablet", files={{src="tablet/main.lua",dest="wire/tablet.lua"}} },
+    full = { label="Full WiRe Rewired Development Package", component=nil, files={{src="server/main.lua",dest="wire/server.lua"},{src="client/main.lua",dest="wire/client.lua"},{src="trigger/main.lua",dest="wire/trigger.lua"},{src="sensor/main.lua",dest="wire/sensor.lua"},{src="tablet/main.lua",dest="wire/tablet.lua"}} },
   },
 }
